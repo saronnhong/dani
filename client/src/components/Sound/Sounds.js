f3f3dcac2843452bc237dcfd772822c4767981e9
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-07-26 13:00:04 -0700

    added a Sound component

---
+++ client/src/components/Sound/Sounds.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import ReactPlayer from 'react-player';
+
+
+class Sound extends Component {
+
+    render() {
+        return (ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" playing />)
+    };
+
+}
+
+export default Sound;

4fd914e17ccd3a54aa300dfba1a82f2b44f745a0
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-07-30 14:17:01 -0700

    styling buttons

--- client/src/components/Subreddits/Subreddits.js
+++ client/src/components/Subreddits/Subreddits.js
@@ -6,9 +6,7 @@ import "./Subreddits.css"
 function Subreddits(props) {
     return (
         <div className="col-md-4">
-            <div class="subButtons">
                 <button type="button" className="btn btn-success" onClick={props.changeSubreddit}>{props.name}</button>
-            </div>
         </div>
     )
 }

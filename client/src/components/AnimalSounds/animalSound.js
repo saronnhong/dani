9d39c90c685ba0d9c9ce4eae82a4e3e71e76aa12
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-07-31 15:16:59 -0700

    brought back Sound component to match  master

--- client/src/components/AnimalSounds/animalSound.js
+++ client/src/components/AnimalSounds/animalSound.js
@@ -1,62 +1,28 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 
-// import animals from "./animalSounds.json";
-// import "./style.css";
+import animals from "./animalSounds.json";
+import "./style.css";
 
-// class AnimalSound extends Component {
-//     state = {
-//         clicked: false
-//     }
-    // onClickFunction(event) {
-    //     setState() => 
-    //     this.setState({
-    //         an
-    //     })
-    // }
+class AnimalSound extends Component {
+    render() {
+        return (
+            // <div className="wrapper">
+                <div className="container">
+                    <div className="row">
+                {animals.map((animal) =>
+                    <div className="col-md-2">
+                        <div className="cardAnimals">
+                            <a href="# " onClick={() => (new Audio(animal.sound)).play()}>
+                                <img className="animalImg"alt="# " src={animal.image} />
+                            </a>
+                        </div>
+                    </div>
+                )}
+                </div>
+                </div>
+            // </div>
+        )
+    }
+}
 
-    // render() {
-    //     return (
-
-    //         <div className="container">
-    //             {/* {this.animals.clicked ? ( */}
-    //                 <div className="row">
-    //                     {animals.map((animal) =>
-    //                         <div className="col-md-2">
-    //                             <div className="cardAnimals">
-    //                                 <a href="# " onClick={() =>
-    //                                 new Audio(animal.sound).stop();
-    //                                   this.setState({animal.clicked : false}) ;
-                                        
-    //                                 >
-    //                                 <img className="animalImg" alt="# " src={animal.image} />
-    //                                 </a>
-    //                             </div>
-    //                         </div>
-    //                 )
-    //                             // }
-    //                 </div>
-    //             // ) :
-                    // (
-                    //     <div className="row">
-                    //         {animals.map((animal) =>
-                    //             <div className="col-md-2">
-                    //                 <div className="cardAnimals">
-                    //                     <a href="# " onClick={() => this.setState({ animal.clicked : true})
-                    //                     (new Audio(animal.sound)).play()}>
-                    //                 <img className="animalImg" alt="# " src={animal.image} />
-                    //                 </a>
-                    //             </div>
-                    //         </div>
-                    //     )}
-                    // </div>
-                    // )
-
-//                 // }
-//             </div>
-//         )
-//     }
-// }
-
-// export default AnimalSound;
-
-
+export default AnimalSound;

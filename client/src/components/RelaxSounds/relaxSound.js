930b3d010d02873ee538ce300e1e974fd60c6ff9
Saron <saronnhong@gmail.com>
2019-07-31 13:35:09 -0700

    working on pause

--- client/src/components/RelaxSounds/relaxSound.js
+++ client/src/components/RelaxSounds/relaxSound.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import animals from "./relaxSounds.json";
 import "./style.css";
 
+var oneSound = (new Audio("https://freesound.org/data/previews/390/390879_197130-lq.mp3"));
+
 class RelaxSounds extends Component {
     render() {
         return (
@@ -11,13 +13,17 @@ class RelaxSounds extends Component {
                 {animals.map((animal) =>
                     <div className="col-md-3">
                         <div className="cardRelax">
-                            <a href="# " onClick={() => (new Audio(animal.sound)).play()}>
+                            <a href="# " onClick={() => oneSound.play()}>
                                 <img className= "relaxImg" alt="" src={animal.image} />
                             </a>
                         </div>
+                        <button onClick={() => {
+                            oneSound.pause();
+                            }}>Stop</button>
                     </div>
                 )}
             </div>
+            
             </div>
         )
     }

7f44963eb2c860bf9e190c72bdb12e588748fea5
Saron <saronnhong@gmail.com>
2019-07-31 16:10:01 -0700

    fix sound button

--- client/src/components/RelaxSounds/relaxSound.js
+++ client/src/components/RelaxSounds/relaxSound.js
@@ -3,27 +3,51 @@ import React, { Component } from "react";
 import animals from "./relaxSounds.json";
 import "./style.css";
 
-var oneSound = (new Audio("https://freesound.org/data/previews/390/390879_197130-lq.mp3"));
+const audioElements = {};
+
+// var oneSound = (new Audio("https://freesound.org/data/previews/390/390879_197130-lq.mp3"));
+
+class Sound extends Component {
+
+    audio = new Audio(this.props.animal.sound)
+
+    playSound = () => {
+        this.audio.play();
+    }
+
+    pauseSound = () => {
+        this.audio.pause();
+    }
+
+    render() {
+        const {animal} = this.props;
+        return (
+            <div key={animal.id} className="col-md-3">
+                <div className="cardRelax">
+                    <div onClick={() => this.playSound()}>
+                        <img className="relaxImg" alt="" src={animal.image} />
+                    </div>
+                </div>
+                <button onClick={() => this.pauseSound()}>Stop</button>
+            </div>
+
+        )
+    }
+}
 
 class RelaxSounds extends Component {
     render() {
         return (
             <div className="container">
-            <div className="row">
-                {animals.map((animal) =>
-                    <div className="col-md-3">
-                        <div className="cardRelax">
-                            <a href="# " onClick={() => oneSound.play()}>
-                                <img className= "relaxImg" alt="" src={animal.image} />
-                            </a>
-                        </div>
-                        <button onClick={() => {
-                            oneSound.pause();
-                            }}>Stop</button>
-                    </div>
-                )}
-            </div>
-            
+                <div className="row">
+                    {animals.map((animal) => {
+                        return (
+                            <Sound animal={animal} />
+                        )
+                    }
+                    )}
+                </div>
+
             </div>
         )
     }

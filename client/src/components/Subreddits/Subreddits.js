428476e9ef20e833be7a27126a973575e513de07
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-07-30 19:08:48 -0700

    added more styling for the reddits page

--- client/src/components/Subreddits/Subreddits.js
+++ client/src/components/Subreddits/Subreddits.js
@@ -6,7 +6,9 @@ import "./Subreddits.css"
 function Subreddits(props) {
     return (
         <div className="col-md-4">
+            <span className="btn-group">
                 <button type="button" className="btn btn-danger" onClick={props.changeSubreddit}>{props.name}</button>
+            </span>
         </div>
     )
 }

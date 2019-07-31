b4ce4115ceeb45cbeb46088e22fa8d4713319159
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-07-31 13:35:42 -0700

    added font awesome link and icons to buttons and reorganized routes to follow a logical sequence

--- client/src/index.js
+++ client/src/index.js
@@ -43,46 +43,48 @@ ReactDOM.render(
             <Route exact path="/relaxSounds" component={RelaxSound} />
             <Route exact path="/videos" component={Video} />
             
-            <Route exact path="/Listen"
-                render={(props) => <ButtonPage {...props}
-                    linkOne="/animalSounds"
-                    linkTwo="/natureSounds"
-                    linkThree="/relaxSounds"
-                    buttonOne="Animals"
-                    buttonTwo="Nature"
-                    buttonThree="Relax"
-                />}
-            />
+            
             <Route exact path="/"
                 render={(props) => <ButtonPage {...props}
                     linkOne="/Look"
                     linkTwo="/Listen"
                     linkThree="/Learn"
-                    buttonOne="Look"
-                    buttonTwo="Listen"
-                    buttonThree="Learn"
+                    buttonOne={<div> <i class="fas fa-eye fa-5x"></i> <p className="font-buttons-text">Look</p> </div> }
+                    buttonTwo={<div> <i class="fas fa-headphones-alt fa-5x"></i> <p className="font-buttons-text">Listen</p> </div> }
+                    buttonThree={<div> <i class="fab fa-leanpub fa-5x"></i> <p className="font-buttons-text">Learn</p> </div> }
+                />}
+            />  
+             <Route exact path="/Look"
+                render={(props) => <ButtonPage {...props}
+                    linkOne="/reddit"
+                    linkTwo="/videos"
+                    linkThree="/TBD"
+                    buttonOne={<div> <i class="fas fa-camera fa-5x"></i> <p className="font-buttons-text">Images</p> </div> }
+                    buttonTwo={<div> <i class="fas fa-video fa-5x"></i> <p className="font-buttons-text">Videos</p> </div> }
+                    buttonThree="TBD"
+                />}
+            />
+            <Route exact path="/Listen"
+                render={(props) => <ButtonPage {...props}
+                    linkOne="/animalSounds"
+                    linkTwo="/natureSounds"
+                    linkThree="/relaxSounds"
+                    buttonOne={<div> <i class="fas fa-paw fa-5x"></i> <p className="font-buttons-text">Animals</p> </div> }
+                    buttonTwo={<div> <i class="fas fa-leaf fa-5x"></i> <p className="font-buttons-text">Nature</p> </div> }
+                    buttonThree={<div> <i class="fas fa-couch fa-5x"></i> <p className="font-buttons-text">Relax</p> </div> }
                 />}
             />
             <Route exact path="/Learn"
                 render={(props) => <ButtonPage {...props}
-                    linkOne="/videos"
+                    linkOne="/TBD"
                     linkTwo="/Color"
                     linkThree="/sketch"
-                    buttonOne="Videos"
-                    buttonTwo="Coloring"
-                    buttonThree="Drawing"
+                    buttonOne="TBD"
+                    buttonTwo={<div> <i class="fas fa-paint-brush fa-5x"></i> <p className="font-buttons-text">Coloring</p> </div> }
+                    buttonThree={<div> <i class="fas fa-palette fa-5x"></i> <p className="font-buttons-text">Drawing</p> </div> }
                 />}
             />
-            <Route exact path="/Look"
-                render={(props) => <ButtonPage {...props}
-                    linkOne="/reddit"
-                    linkTwo="/TBD"
-                    linkThree="/TBD"
-                    buttonOne="Images"
-                    buttonTwo="TBD"
-                    buttonThree="TBD"
-                />}
-            />
+           
 
         </div>
     </Router>

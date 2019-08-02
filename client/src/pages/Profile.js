68cd27a521a9dbdba041e2364182852496bb5b00
Max Patten <amanthus2535@gmail.com>
2019-08-01 18:03:45 -0700

    got first version of visitation tracking working... profile will now display # of visits to profile upon click of display button

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import withAuth from './../components/withAuth';
 import API from './../utils/API';
-import { Link } from 'react-router-dom';
 
 class Profile extends Component {
 
   state = {
     username: "",
     email: "",
-    metric: ""
+    metricID: "",
+    metrics: ""
   };
 
   componentDidMount() {
@@ -16,18 +16,18 @@ class Profile extends Component {
       this.setState({
         username: res.data.username,
         email: res.data.email,
-        metric: res.data.metric
+        metricID: res.data.metric
       })
 
     });
   }
 
   readMetrics() {
-    API.getMetrics(this.state.metric).then(res =>
+    API.getMetrics(this.state.metricID).then(res => {
       this.setState({
-        metrics: res.data
+        metrics: Object.values(res.data[0])[0]
       })
-      )
+    })
   }
 
   render() {
@@ -35,7 +35,8 @@ class Profile extends Component {
       <div className="container Profile">
         <h1>Welcome to your profile {this.state.username}</h1>
         <p>Your Email: {this.state.email}</p>
-        <button onClick={()=> this.readMetrics}>Show my Stats!</button>
+        <button onClick={() => this.readMetrics()}>Show my Stats!</button>
+        <p>Visits to profile: {this.state.metrics}</p>
       </div>
     )
   }

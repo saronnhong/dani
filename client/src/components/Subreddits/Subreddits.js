fc47ba9de757512e00118b270b34dce1f47f70a4
Max Patten <amanthus2535@gmail.com>
2019-08-01 17:21:52 -0700

    Fixed a few react warnings from missing titles and alts, also worked on getting metric routes set up

--- client/src/components/Subreddits/Subreddits.js
+++ client/src/components/Subreddits/Subreddits.js
@@ -6,7 +6,11 @@ import "./Subreddits.css"
 function Subreddits(props) {
     return (
         <div className="col-md-4">
-                <button type="button" className="btn btn-danger subButton mt-3 btn-block" onClick={props.changeSubreddit}>{props.name} <img className="buttonIcon" src={props.icon}></img></button>
+            <button type="button"
+                className="btn btn-danger subButton mt-3 btn-block"
+                onClick={props.changeSubreddit}>{props.name}
+                <img alt={props.name + "image"} className="buttonIcon" src={props.icon}>
+                </img></button>
         </div>
     )
 }

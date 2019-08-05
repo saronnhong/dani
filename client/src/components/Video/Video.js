a67450d0e5b68b3d86d5f928295171c51d1bc1d8
Saron <saronnhong@gmail.com>
2019-08-05 13:51:22 -0700

    testing center videos

--- client/src/components/Video/Video.js
+++ client/src/components/Video/Video.js
@@ -1,21 +1,17 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-
 import ReactPlayer from 'react-player'
 import videos from "./videos.json";
 import Back from "../Back"
 import API from './../../utils/API';
 import withAuth from './../withAuth';
 import "./style.css";
-
-
 class Video extends Component {
     state = {
         playing: false,
         metricID: "",
         metrics: []
     }
-
     componentDidMount() {
         API.getUser(this.props.user.id).then(res => {
             this.setState({
@@ -25,46 +21,48 @@ class Video extends Component {
             API.addToMetrics(res.data.metric, pageOn)
         });
     }
-
     render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-md">
-
                         {/* turnary */}
                         {this.state.playing ? (
                             <div className="container-fluid">
                                 <div className="row">
-                                    <div className="wrapper wrap-background">
-                                        {videos.map((video) =>
-                                            <div key={video.name} className="video-wrapper">
-                                                <p className="video-name">{video.name}</p>
-                                                <div>
-                                                    <ReactPlayer onClick={() =>
-                                                        this.setState({ playing: false })
-                                                    }
-                                                        url={video.video} playing />
-                                                </div>
-                                            </div>
-                                        )}
-                                    </div>
-                                </div>
-                            </div>) : (
-                                <div className="container-fluid">
-                                    <div className="row">
+                                    <div className="col-lg-12 videoColumn">
                                         <div className="wrapper wrap-background">
                                             {videos.map((video) =>
                                                 <div key={video.name} className="video-wrapper">
                                                     <p className="video-name">{video.name}</p>
                                                     <div>
                                                         <ReactPlayer onClick={() =>
-                                                            this.setState({ playing: true })
+                                                            this.setState({ playing: false })
                                                         }
-                                                            url={video.video} />
+                                                            url={video.video} playing />
                                                     </div>
                                                 </div>
                                             )}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>) : (
+                                <div className="container-fluid">
+                                    <div className="row">
+                                        <div className="col-lg-12 videoColumn">
+                                            <div className="wrapper wrap-background">
+                                                {videos.map((video) =>
+                                                    <div key={video.name} className="video-wrapper">
+                                                        <p className="video-name">{video.name}</p>
+                                                        <div>
+                                                            <ReactPlayer onClick={() =>
+                                                                this.setState({ playing: true })
+                                                            }
+                                                                url={video.video} />
+                                                        </div>
+                                                    </div>
+                                                )}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -84,5 +82,4 @@ class Video extends Component {
         )
     }
 }
-
 export default withAuth(Video);

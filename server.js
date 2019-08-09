1c824b3d13787a5112cd34acf6d882a5bdfd8e5d
Saron <saronnhong@gmail.com>
2019-08-09 12:52:53 -0700

    add personal mlab user info for connect to heroku

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 mongoose
    // for the app to work as a deployed app with mLAB MongoDB provision on Heroku, use:
-   .connect(process.env.MONGODB_URI || 'mongodb://user:password1@ds045598.mlab.com:45598/heroku_xt21mbhz', { useNewUrlParser: true, useCreateIndex: true })
+   .connect(process.env.MONGODB_URI || 'mongodb://user:password123@ds127958.mlab.com:27958/heroku_wb6nc7kg', { useNewUrlParser: true, useCreateIndex: true })
    // for LOCAL Testing, use:
-   // .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/appDB', { useNewUrlParser: true, useCreateIndex: true })
+  //  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/appDB', { useNewUrlParser: true, useCreateIndex: true })
   .then(() => console.log("MongoDB Connected!"))
   .catch(err => console.error(err));
 
